243b0f2c130a42bd96e8bda258c10270d92562a5
duloti53 <Elizabeth.J.Brown343498@gmail.com>
2021-12-17 17:25:09 +0800

    add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '1440px',
+      // => @media (min-width: 960px) { ... }
+    },
+    colors: {
+      'brown': '#89685F',
+      'brown-light': '#BD866A',
+      'black': '#4F494C',
+      'pink': '#FD72AD',
+      'orange': '#FCCE9E',
+      'blue-light': '#CFE3FF',
+      'yellow-light': '#FCCE9E',
+      'purple-light': '#A561FF',
+      'gray-light': '#F4ECDC',
+    },
     extend: {},
   },
   plugins: [],
