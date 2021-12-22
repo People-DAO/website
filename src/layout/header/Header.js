a12ac0092c3e46688b2ce469dde9801244456892
bartosjiri <me@bartosjiri.com>
2021-12-22 14:28:44 +0100

    Add header, hero & minor updates

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,14 +1,45 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Headroom from 'react-headroom'
 import Fade from 'react-reveal/Fade'
+import SVG from "react-inlinesvg"
+import {Link as ScrollLink} from 'react-scroll'
 
 import * as headerStyle from './Header.module.scss'
+
+import socials from "../../../configuration/socials.yml"
 
 const Header = () => (
   <Headroom>
     <Fade top>
-      <header className={headerStyle.header}>Header</header>
+      <header className={headerStyle.header}>
+        <div className={headerStyle.container}>
+          <div className={headerStyle.logo}>
+            <ScrollLink to="top" smooth>
+              <SVG src="/assets/icons/peopledao/logo.svg" />
+            </ScrollLink>
+          </div>
+          <nav className={headerStyle.navigation}>
+            <ScrollLink to="about" smooth>
+              <span>History</span>
+            </ScrollLink>
+            <ScrollLink to="token" smooth>
+              <span>$PEOPLE</span>
+            </ScrollLink>
+            <ScrollLink to="projects" smooth>
+              <span>Projects</span>
+            </ScrollLink>
+            <ScrollLink to="proposals" smooth>
+              <span>Proposals</span>
+            </ScrollLink>
+            <ScrollLink to="faq" smooth>
+              <span>FAQ</span>
+            </ScrollLink>
+            <a className={headerStyle.action} href={socials.discordUrl} target="_blank">
+              <span>Join Discord</span>
+            </a>
+          </nav>
+        </div>
+      </header>
     </Fade>
   </Headroom>
 )
--- src/sections/index/proposals/Proposals.js
+++ src/sections/index/proposals/Proposals.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 
 import Section from "../../../components/section/Section"
 import Button from "../../../components/button/Button"
@@ -7,25 +8,31 @@ import * as proposalsStyle from "./Proposals.module.scss"
 
 const ProposalsSection = () => (
   <Section name="proposals" className={proposalsStyle.proposals}>
-    <div className={proposalsStyle.title}>
-      <h3>Proposals in snapshot</h3>
-    </div>
-    <div className={proposalsStyle.list}>
-      <div className={proposalsStyle.item} />
-      <div className={proposalsStyle.item} />
-      <div className={proposalsStyle.item} />
-      <div className={proposalsStyle.item} />
-    </div>
-    <div className={proposalsStyle.action}>
-      <Button
-        type="secondary"
-        className={proposalsStyle.button}
-        href="@TODO"
-        target="_blank"
-      >
-        <span>Download proposal template</span>
-      </Button>
-    </div>
+    <Fade bottom distance="25%">
+      <div className={proposalsStyle.title}>
+        <h3>Proposals in snapshot</h3>
+      </div>
+    </Fade>
+    <Fade distance="25%">
+      <div className={proposalsStyle.content}>
+        <div className={proposalsStyle.list}>
+          <div className={proposalsStyle.item} />
+          <div className={proposalsStyle.item} />
+          <div className={proposalsStyle.item} />
+          <div className={proposalsStyle.item} />
+        </div>
+        <div className={proposalsStyle.action}>
+          <Button
+            type="secondary"
+            className={proposalsStyle.button}
+            href="@TODO"
+            target="_blank"
+          >
+            <span>Download proposal template</span>
+          </Button>
+        </div>
+      </div>
+    </Fade>
   </Section>
 )
 
