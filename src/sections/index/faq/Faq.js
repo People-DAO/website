cc3857fa66206cf2b575fa892e5df703f5ece865
bartosjiri <me@bartosjiri.com>
2021-12-27 17:40:48 +0100

    Update FAQ section

--- src/sections/index/faq/Faq.js
+++ src/sections/index/faq/Faq.js
@@ -1,27 +1,36 @@
 import React from "react"
+import Fade from "react-reveal/Fade"
 
 import Section from "../../../components/section/Section"
+import Accordion from "../../../components/accordion/Accordion"
 
 import * as faqStyle from "./Faq.module.scss"
 
+import content from "../../../../content/index/faq.yml"
+
 const FaqSection = () => (
   <Section name="faq" className={faqStyle.faq}>
-    <div>
-      <div className={faqStyle.wrapper}>
-        <div className={faqStyle.title}>
-          <h1>FAQ section</h1>
-          <span>frequently asked questions</span>
-          </div>
+    <div className={faqStyle.wrapper}>
+      <Fade distance="25%">
         <div className={faqStyle.content}>
-          <div className={faqStyle.item}>What is PeopleDAO?</div>
-          <div className={faqStyle.item}>What is PeopleDAO?</div>
-          <div className={faqStyle.item}>What is PeopleDAO?</div>
-          <div className={faqStyle.item}>What is PeopleDAO?</div>
-          <div className={faqStyle.item}>What is PeopleDAO?</div>
+          <div className={faqStyle.title}>
+            <h3>FAQ</h3>
+            <span>Frequently asked questions</span>
+          </div>
+          <div className={faqStyle.list}>
+            {content?.length && content.map((item, index) => (
+              <Accordion
+                title={item?.question}
+                className={faqStyle.item}
+                open={!index}
+              >
+                <p>{item?.answer}</p>
+              </Accordion>
+            ))}
+          </div>
         </div>
-      </div>
+      </Fade>
     </div>
-    {/* <span>FAQ section</span> */}
   </Section>
 )
 
