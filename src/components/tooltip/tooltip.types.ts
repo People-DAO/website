0e11fa509896778b5fc318d673160a747ec42421
bartosjiri <me@bartosjiri.com>
2022-03-27 20:13:27 +0200

    Add tooltip component

---
+++ src/components/tooltip/tooltip.types.ts
@@ -0,0 +1,20 @@
+export type TooltipPlacement =
+  | 'auto'
+  | 'auto-start'
+  | 'auto-end'
+  | 'top'
+  | 'top-start'
+  | 'top-end'
+  | 'bottom'
+  | 'bottom-start'
+  | 'bottom-end'
+  | 'right'
+  | 'right-start'
+  | 'right-end'
+  | 'left'
+  | 'left-start'
+  | 'left-end';
+
+export type TooltipStrategy = 'absolute' | 'fixed';
+
+export type TooltipBackground = "primary" | "secondary" | "tertiary";
