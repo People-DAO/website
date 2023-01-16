473ab884c35d9d5e8476509542bafc61028018ec
bartosjiri <me@bartosjiri.com>
2023-01-16 23:10:24 +0100

    Add announcements banner components

--- src/lib/layout/stores/viewport.store.ts
+++ src/lib/layout/stores/viewport.store.ts
@@ -6,9 +6,9 @@ export type Viewport = 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 
 const orderedViewports = ['sm', 'md', 'lg', 'xl', '2xl'];
 
-export let viewport: Writable<Viewport> = writable('sm');
+export const viewport: Writable<Viewport> = writable('sm');
 
-export let isViewportGTE = (_: Viewport, targetViewport: Viewport) => {
+export const isViewportGTE = (_: Viewport, targetViewport: Viewport) => {
 	const currentViewport = get(viewport);
 	const currentViewportIndex = orderedViewports.indexOf(currentViewport);
 	const targetViewportIndex = orderedViewports.indexOf(targetViewport);
